8040e9232c1cb1159e5155cb2ef9b4935b693691
Sreedevi Ravindranadhan <sreedeviravindranadhan@gmail.com>
2022-12-13 23:46:00 -0600

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,6 +2,7 @@ import { Card, ListItem, Avatar } from "react-native-elements";
 import { ScrollView, Text } from "react-native";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 const Mission = () => {
     return (
@@ -18,21 +19,46 @@ const Mission = () => {
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
 
+    if (partners.isLaoding) {
+        return(
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+    if(partners.errMess) {
+        return(
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+        
     return (
         <ScrollView>
             <Mission />
             <Card>
-               <Card.Title>Community Partners</Card.Title>
-               <Card.Divider />
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider />
                {partners.partnersArray.map((partner) => (
-                      <ListItem key={partner.id}>
-                      <Avatar source ={{ uri: baseUrl + partner.image }} rounded />
+                    <ListItem key={partner.id}>
+                    <Avatar source ={{ uri: baseUrl + partner.image }} rounded />
                         <ListItem.Content>
                           <ListItem.Title>{partner.name}</ListItem.Title>
                           <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
                         </ListItem.Content>
-                      </ListItem>
-                     ))}
+                    </ListItem>
+                ))}
             </Card>
         </ScrollView>
     );

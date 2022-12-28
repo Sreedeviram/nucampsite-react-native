6a49ac4d2b6f3dac4a3fad8a0f8ef953a0578659
Sreedevi Ravindranadhan <sreedeviravindranadhan@gmail.com>
2022-12-27 22:36:49 -0600

    Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { Text, View, ScrollView, StyleSheet, Switch, Button, Alert } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import * as Animatable from 'react-native-animatable'
+import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
@@ -21,6 +22,34 @@ const ReservationScreen = () => {
         setHikeIn(false);
         setDate(new Date());
         setShowCalendar(false);
+    };
+
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            });
+        };
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     };
 
     return(
@@ -88,7 +117,12 @@ const ReservationScreen = () => {
                                 },
                                 {
                                     text: 'OK',
-                                    onPress: () => resetForm()
+                                    onPress: () => {
+                                        presentLocalNotification(
+                                            date.toLocaleDateString('en-US')
+                                        );
+                                    resetForm();
+                                    }
                                 }
                             ],
                             { cancelable: false }
